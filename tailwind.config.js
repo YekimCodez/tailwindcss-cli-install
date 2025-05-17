a1ddef42f915fc878d3f7e0e3268de45d11e7833
Michael Doughty <yekimcodes@gmail.com>
2025-05-17 13:40:01 -0400

    Added some code to the tailwind config file for a custom container class

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+        },
+      },
+    },
+  },
+};
